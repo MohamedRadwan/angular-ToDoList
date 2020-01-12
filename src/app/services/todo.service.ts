a9b502391db2c4ecfa1f2adba5906b9e6ec568a6
Mohamed Radwan <mohamed.rad@outlook.com>
2020-01-11 21:15:51 -0500

    added serivces

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,28 +1,29 @@
 import { Injectable } from "@angular/core";
+import { HttpClient,HttpHeaders} from '@angular/common/http';
+import {Todo} from '../models/Todo';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type' : 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: "root"
 })
 export class TodoService {
-  constructor() {}
+  todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit: string = '?_limit=5';
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: "Todo One",
-        completed: false
-      },
-      {
-        id: 2,
-        title: "Todo Two",
-        completed: true
-      },
-      {
-        id: 3,
-        title: "Todo Three",
-        completed: false
-      }
-    ];
+  constructor(private http:HttpClient) {}
+
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+
+  togglecompleted(todo:Todo):Observable<>{
+    return this.http.put(url,todo,httpOptions);
   }
 }
