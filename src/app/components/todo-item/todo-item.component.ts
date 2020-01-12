8c89b8580183b61852dbfe228acbc879bf504ecd
Mohamed Radwan <mohamed.rad@outlook.com>
2020-01-11 19:10:30 -0500

    added service

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,34 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Todo } from 'src/app/models/Todo';
+import { Component, OnInit, Input } from "@angular/core";
+import { Todo } from "src/app/models/Todo";
 
 @Component({
-  selector: 'app-todo-item',
-  templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  selector: "app-todo-item",
+  templateUrl: "./todo-item.component.html",
+  styleUrls: ["./todo-item.component.css"]
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   //set dynamic classes
   setClasses() {
     let classes = {
       todo: true,
-      'is-complete': this.todo.completed
-    }
+      "is-complete": this.todo.completed
+    };
     return classes;
   }
 
   onToggle(todo) {
-
+    todo.completed = !todo.completed;
   }
 
-  onDelete(todo) {
-
-  }
-
+  onDelete(todo) {}
 }
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,33 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Todo } from '../../models/Todo';
+import { Component, OnInit } from "@angular/core";
+import { Todo } from "../../models/Todo";
+import { TodoService } from "../../services/todo.service";
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.css"]
 })
 export class TodosComponent implements OnInit {
   todos: Todo[];
-  constructor() { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ]
+    this.todos = this.todoService.getTodos();
   }
-
 }
